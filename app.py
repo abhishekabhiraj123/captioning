1f400c32cf1be479dc87cce329c84fa1283d85ba
abhishekabhiraj123 <79674559+abhishekabhiraj123@users.noreply.github.com>
2024-05-02 18:55:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def query(uploaded_file):
         return []
 
 # Streamlit app
-st.title("Image Captioning with Hugging Face Model")
+st.title("Image Captioning with blip image-captioning Large Moacha Model")
 st.markdown("Upload an image, and this app will generate captions for it using the image captioning model.")
 
 # Upload image
